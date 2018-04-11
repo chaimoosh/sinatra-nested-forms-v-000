741a4d54928ff5f97a731d2f729988e0fdfa27aa
Chaim Reisman <crreisman@gmail.com>
2018-04-11 23:30:28 +0000

    automatically backed up by learn

--- models/ship.rb
+++ models/ship.rb
@@ -2,11 +2,7 @@ class Ship
   attr_reader :name, :type, :booty
   
   SHIPS = []
-<<<<<<< HEAD
-  def initialize(args)
-=======
   def initalize(args)
->>>>>>> 80e3f0d3f631c5879653f48d9494cfc402c899dc
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
